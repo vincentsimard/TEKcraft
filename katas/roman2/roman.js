c4af294288a7e21029e465de73593c0914c2207f
vincentsimard <vincent@vincentsimard.com>
2012-11-01 18:23:11 -0400

    Completed roman numbers kata practice #2

--- katas/roman2/roman.js
+++ katas/roman2/roman.js
@@ -1,7 +1,38 @@
 'use strict';
 
-function intToRoman() {
+function intToRoman(number) {
+  var result = '';
+  var symbols = {
+    'M':  1000,
+    'CM':  900,
+    'D':   500,
+    'CD':  400,
+    'C':   100,
+    'XC':   90,
+    'L':    50,
+    'XL':   40,
+    'X':    10,
+    'IX':    9,
+    'V':     5,
+    'IV':    4,
+    'I':     1
+  };
 
+  var rest = number;
+  var key;
+  var value;
+
+  if (typeof number === 'undefined' || number === 0) { return result; }
+
+  for (key in symbols) {
+    value = symbols[key];
+    while (rest >= value) {
+      result += key;
+      rest -= value;
+    }
+  }
+
+  return result;
 }
 
 module.exports.intToRoman = intToRoman;
--- katas/roman2/test/roman.test.js
+++ katas/roman2/test/roman.test.js
@@ -2,6 +2,52 @@
   'use strict';
 
   var chai = require('chai');
-  var expect = chai.expect;
+  var should = chai.should();
+
   var intToRoman = require('./../roman').intToRoman;
+
+
+  describe('intToRoman()', function() {
+    it('should be defined', function() {
+      intToRoman.should.not.be.undefined;
+      intToRoman.should.be.a('function');
+    });
+
+    it('should return "" if no arguments are passed or 0', function() {
+      intToRoman().should.be.empty;
+      intToRoman(0).should.be.empty;
+    });
+
+    it('should return "M" for 1000', function() {
+      intToRoman(1000).should.equal("M");
+    });
+
+    it('should return "MM" for 2000', function() {
+      intToRoman(2000).should.equal("MM");
+    });
+
+    it('should return "CM" for 900', function() {
+      intToRoman(900).should.equal("CM");
+    });
+
+    it('should return "D" for 500', function() {
+      intToRoman(500).should.equal("D");
+    });
+
+    it('should return "CD" for 400', function() {
+      intToRoman(400).should.equal("CD");
+    });
+
+    it('should return "C" for 100', function() {
+      intToRoman(100).should.equal("C");
+    });
+
+    it('should return "MCMXCIX" for 1999', function() {
+      intToRoman(1999).should.equal("MCMXCIX");
+    });
+
+    it('should return "MMMMCMXLVI" for 4946', function() {
+      intToRoman(4946).should.equal("MMMMCMXLVI");
+    });    
+  });
 })();
