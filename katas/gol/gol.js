e6daf9ea3e33729fdba0409de2b8d82620f04e30
vincentsimard <vincent@vincentsimard.com>
2012-11-02 18:18:33 -0400

    Some progress on the Game of Life kata

--- katas/gol/gol.js
+++ katas/gol/gol.js
@@ -1,10 +1,42 @@
 'use strict';
 
-var GameOfLife = (function() {
-  // Constructor
-  function GameOfLife() {}
 
-  return GameOfLife;
-})();
+
+function setupGrid(x, y) {
+  var grid = new Array(x);
+
+  for (var i = 0; i < grid.length; i++) {
+    grid[i] = new Array(y);
+  }
+
+  return grid;
+}
+
+
+
+// Constructor
+var GameOfLife = function(gridDimensions) {
+  // Parameters validation
+  if (typeof gridDimensions !== 'undefined') {
+    if (isNaN(gridDimensions)) {
+      if (!(gridDimensions instanceof Array)) { throw new Error(); }
+      if (isNaN(gridDimensions[0]) || isNaN(gridDimensions[1])) { throw new Error(); }
+    }
+  }
+
+  // Default parameters values
+  gridDimensions = gridDimensions || [8,8];
+
+  // Grid setup
+  if (isNaN(gridDimensions)) {
+    this.grid = setupGrid(gridDimensions[0], gridDimensions[1]);
+  } else {
+    this.grid = setupGrid(gridDimensions, gridDimensions);
+  }
+};
+
+GameOfLife.prototype.step = function() {};
+
+
 
 module.exports.GameOfLife = GameOfLife;
--- katas/gol/test/gol.test.js
+++ katas/gol/test/gol.test.js
@@ -11,11 +11,95 @@
       GameOfLife.should.not.be.undefined;
     });
 
-    // describe('step()', function() {
-    //   it('should be defined', function() {
-    //     GameOfLife.step.should.not.be.undefined;
-    //     GameOfLife.step.should.be.a('function');
-    //   });
-    // });
+
+
+    describe('arguments', function() {
+      it('should be optional', function() {
+        var fnNoArguments = (function() {
+          new GameOfLife();
+        });
+
+        fnNoArguments.should.not.throw(Error);
+      });
+
+      describe('first argument', function() {
+        it('should throw if it is not: falsy, a number, or an array of 2 numbers', function() {
+          var fnValid = (function() {
+            new GameOfLife([1,2]);
+            new GameOfLife(1);
+          });
+
+          var fnUndefined = (function() {
+            new GameOfLife();
+            new GameOfLife(undefined);
+          });
+
+          var fnInvalid1 = (function() { new GameOfLife('a'); });
+          var fnInvalid2 = (function() { new GameOfLife([1, 'a']); });
+          var fnInvalid3 = (function() { new GameOfLife({}); });
+
+          fnValid.should.not.throw(Error);
+          fnUndefined.should.not.throw(Error);
+          fnInvalid1.should.throw(Error);
+          fnInvalid2.should.throw(Error);
+          fnInvalid3.should.throw(Error);
+        });
+
+        it('should set the grid dimensions when receiving an array of 2 numbers', function() {
+          function testGridDimensions(dimensions) {
+            var gol = new GameOfLife(dimensions);
+            var xLen = gol.grid.length;
+            var yLen = gol.grid[0].length;
+
+            xLen.should.equal(dimensions[0]);
+            yLen.should.equal(dimensions[1]);
+          }
+
+          testGridDimensions([2,2]);
+          testGridDimensions([25,32]);
+        });
+
+        it('should set the grid dimensions when receiving a number', function() {
+          function testGridDimensions(dimensions) {
+            var gol = new GameOfLife(dimensions);
+            var xLen = gol.grid.length;
+            var yLen = gol.grid[0].length;
+
+            xLen.should.equal(dimensions);
+            yLen.should.equal(dimensions);
+          }
+
+          testGridDimensions(9);
+          testGridDimensions(12);
+        });
+      });
+    });
+
+
+
+    describe('grid', function() {
+      it('should be defined as an array', function() {
+        new GameOfLife().grid.should.not.be.undefined;
+        new GameOfLife().grid.should.be.instanceof(Array);
+      });
+
+      it('should have 8x8 dimensions by default', function() {
+        var gol = new GameOfLife();
+        var xLen = gol.grid.length;
+        var yLen = gol.grid[0].length;
+
+        xLen.should.equal(8);
+        yLen.should.equal(8);
+      });
+    });
+
+
+
+    describe('step()', function() {
+      it('should be defined as a function', function() {
+        new GameOfLife().step.should.not.be.undefined;
+        new GameOfLife().step.should.be.a('function');
+      });
+    });
   });
 })();
