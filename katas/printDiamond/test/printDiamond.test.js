281628c9009ed4e205dd1eaf7174a8ef481b61a4
vincentsimard <vincent@vincentsimard.com>
2012-11-26 17:18:45 -0500

    printDiamond: Refactoring tests to be less specific

--- katas/printDiamond/test/printDiamond.test.js
+++ katas/printDiamond/test/printDiamond.test.js
@@ -6,6 +6,8 @@
 
   var printDiamond = require('./../printDiamond').printDiamond;
 
+
+
   describe('printDiamond', function() {
     it('should be defined', function() {
       printDiamond.should.not.be.undefined;
@@ -28,6 +30,40 @@
     });
 
     describe('with argument "char"', function() {
+      it('returned line length should be equal to: (position of the char in the alphabet (zero-based)) * 2 + 1', function() {
+        function getMaxLength(char) {
+          var result = printDiamond(char);
+          var lines = result.split('\n').clean('');
+          return Math.max.apply(Math, lines.map(function (e) { return e.length }));
+        }
+
+        getMaxLength('A').should.equal(1);
+        getMaxLength('B').should.equal(3);
+        getMaxLength('C').should.equal(5);
+      });
+
+      it('each returned line should have the same length', function() {
+        function hasSameLineLength(char) {
+          var result = printDiamond(char);
+          var lines = result.split('\n').clean('');
+          var ret = true;
+
+          if (lines.length < 2) { return true; }
+
+          for (var i = 1; i < lines.length; i++) {
+            ret = ret && (lines[0].length === lines[i].length);
+          }
+
+          return ret;
+        }
+
+        hasSameLineLength('A').should.be.true;
+        hasSameLineLength('B').should.be.true;
+        hasSameLineLength('C').should.be.true;
+      });
+
+
+/*
       describe('"A"', function() {
         it('should return "A"', function() {
           printDiamond('A').should.equal('A');
@@ -45,6 +81,19 @@
           printDiamond('C').should.equal('  A  \n B B \nC   C\n B B \n  A  \n');
         });
       });
+*/
     });
   });
+
+
+
+  Array.prototype.clean = function(deleteValue) {
+    for (var i = 0; i < this.length; i++) {
+      if (this[i] == deleteValue) {         
+        this.splice(i, 1);
+        i--;
+      }
+    }
+    return this;
+  };
 })();
