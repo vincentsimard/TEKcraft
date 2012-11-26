02dbc04799b562e375a85bbfd1234b62dc4911b1
vincentsimard <vincent@vincentsimard.com>
2012-11-26 16:07:55 -0500

    printDiamond: Refactoring the parameter validation

--- katas/printDiamond/printDiamond.js
+++ katas/printDiamond/printDiamond.js
@@ -6,8 +6,11 @@ var printDiamond = function(char) {
 
   // Parse the char parameter as a string and convert to upperstring
   char = (char + '').toUpperCase();
-  if (char.length !== 1) { throw new TypeError(); }
-  if (char.charCodeAt(0) < 65 || char.charCodeAt(0) > 90) { throw new TypeError(); }
+  if (!isALetter(char)) { throw new TypeError(); }
 };
+
+function isALetter(str) {
+  return !!str.match(/^[A-Za-z]$/);
+}
 
 module.exports.printDiamond = printDiamond;
