51f4855ae96395d92b3af04be5bd5f05b9fd2e46
vincentsimard <vincent@vincentsimard.com>
2012-12-18 15:48:22 -0500

    Completed the Roman Numerals kata in php (toArabic). sortof

--- katas/romanphp/roman.php
+++ katas/romanphp/roman.php
@@ -1,5 +1,32 @@
 <?php
 
 function toArabic($roman) {
-  return 1;
+  $result = 0;
+  
+  $addSymbols = array(
+    'M' => 1000,
+    'D' => 500,
+    'C' => 100,
+    'L' => 50,
+    'X' => 10,
+    'V' => 5,
+    'I' => 1
+  );
+
+  $substractSymbols = array(
+    'IV' => -2*$addSymbols['I'],
+    'IX' => -2*$addSymbols['I'],
+    'XL' => -2*$addSymbols['X'],
+    'XC' => -2*$addSymbols['X'],
+    'CD' => -2*$addSymbols['C'],
+    'CM' => -2*$addSymbols['C']
+  );
+
+  $symbols = array_merge($addSymbols, $substractSymbols);
+
+  foreach ($symbols as $symbol => $value) {
+    $result += substr_count($roman, $symbol) * $value;
+  }
+
+  return $result;
 }
--- katas/romanphp/roman.test.php
+++ katas/romanphp/roman.test.php
@@ -5,7 +5,61 @@ require_once "roman.php";
 
 class RomanTest extends PHPUnit_Framework_TestCase
 {
-  public function testIShouldReturn1() {
-    $this->assertEquals(prime('I'), 1);
+  public function testEmptyStringReturns0() {
+    $this->assertEquals(toArabic(''), 0);
   }
+
+  public function testSingleSymbol() {
+    $this->assertEquals(toArabic('I'), 1);
+    $this->assertEquals(toArabic('V'), 5);
+    $this->assertEquals(toArabic('X'), 10);
+    $this->assertEquals(toArabic('L'), 50);
+    $this->assertEquals(toArabic('C'), 100);
+    $this->assertEquals(toArabic('D'), 500);
+    $this->assertEquals(toArabic('M'), 1000);
+  }
+
+  public function testMultipleSymbols() {
+    $this->assertEquals(toArabic('II'), 2);
+    $this->assertEquals(toArabic('III'), 3);
+
+    $this->assertEquals(toArabic('XX'), 20);
+    $this->assertEquals(toArabic('XXX'), 30);
+
+    $this->assertEquals(toArabic('MM'), 2000);
+    $this->assertEquals(toArabic('MMM'), 3000);
+  }
+
+  public function testMultiplesSymbolsThatCanBeAddedUp() {
+    $this->assertEquals(toArabic('VI'), 6);
+    $this->assertEquals(toArabic('VII'), 7);
+
+    $this->assertEquals(toArabic('XII'), 12);
+    $this->assertEquals(toArabic('XVIII'), 18);
+
+    $this->assertEquals(toArabic('MMMDCCLXVIII'), 3768);
+  }
+
+  public function testIbeforeVorXShouldSubtract1() {
+    $this->assertEquals(toArabic('IV'), 4);
+    $this->assertEquals(toArabic('IX'), 9);
+    $this->assertEquals(toArabic('XIV'), 14);
+  }
+
+  public function testXbeforeLorCShouldSubstract10() {
+    $this->assertEquals(toArabic('XL'), 40);
+    $this->assertEquals(toArabic('XC'), 90);
+    $this->assertEquals(toArabic('MXCI'), 1091);
+  }
+
+  public function testCbeforeDorMShouldSubstract100() {
+    $this->assertEquals(toArabic('CD'), 400);
+    $this->assertEquals(toArabic('CM'), 900);
+  }
+
+  public function testRandomNumbers() {
+    $this->assertEquals(toArabic('MMMMDCCXXXII'), 4732);
+    $this->assertEquals(toArabic('MCMXLIV'), 1944);
+    $this->assertEquals(toArabic('MMCCCXLV'), 2345);
+  }  
 }
