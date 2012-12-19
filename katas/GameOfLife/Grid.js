61010787e3a1ec19bfdaad87bb30fd4d37f7d034
Vincent-x2 <vincent@ubuntu.ubuntu-domain>
2012-12-19 02:14:22 -0500

    Moar Game Of Life

--- katas/GameOfLife/Grid.js
+++ katas/GameOfLife/Grid.js
@@ -1,29 +1,91 @@
-'use strict';
+(function () {
+  'use strict';
 
-var Grid = function() {
-  var args = Array.prototype.slice.call(arguments, 0);
-
-  this.liveCells = args;
-};
-
-Grid.prototype.spawn = function() {
-  return new Grid();
-};
-
-Grid.prototype.neighborsTo = function(x,y) {
-  var neighbors = [];
-  var dx, dy, sameCell;
-
-  for (var i = 0; i < this.liveCells.length; i++) {
-    dx = Math.abs(x-this.liveCells[i][0]);
-    dy = Math.abs(y-this.liveCells[i][1]);
-    sameCell = (dx + dy === 0);
-
-    if (dx <= 1 && dy <= 1 && !sameCell) {
-      neighbors.push(this.liveCells[i]);
+  Array.prototype.containsArray = function(val) {
+    var hash = {};
+    for(var i=0; i < this.length; i++) {
+      hash[this[i]] = i;
     }
-  }
-  return neighbors.length;
-};
+    
+    return hash.hasOwnProperty(val);
+  };
 
-module.exports.Grid = Grid;
+  var Grid = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    var dimensions = [[], []];
+
+    this.liveCells = args;
+    this.width = 0;
+    this.height = 0;
+
+    for (var i = 0; i < this.liveCells.length; i++) {
+      dimensions[0].push(this.liveCells[i][0]);
+      dimensions[1].push(this.liveCells[i][1]);
+    }
+
+    if (this.liveCells.length > 0) {
+      this.maxX = Math.max.apply(null, dimensions[0]);
+      this.minX = Math.min.apply(null, dimensions[0]);
+      this.maxY = Math.max.apply(null, dimensions[1]);
+      this.minY = Math.min.apply(null, dimensions[1]);
+
+      this.width = this.maxX - this.minX + 1;
+      this.height = this.maxY - this.minY + 1;
+    }
+  };
+
+  Grid.prototype.spawn = function() {
+    var grid = new Grid();
+    var survivors = [];
+    var cell, neighbors;
+
+    // if (this.liveCells.length > 0) {
+    //   for (var i = 0; i < this.width; i++) {
+    //     for (var j = 0; j < this.height; j++) {
+    //       cell = [i, j];
+    //       neighbors = this.neighborsTo(cell[0], cell[1]);
+
+    //       if (this.liveCells.containsArray(cell)) {
+    //         if (neighbors === 2 || neighbors === 3) {
+    //           survivors.push(cell);
+    //         }
+    //       } else {
+    //         if (neighbors === 3) {
+    //           survivors.push(cell);
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+
+    for (var i = 0; i < this.liveCells.length; i++) {
+      cell = this.liveCells[i];
+      neighbors = this.neighborsTo(cell[0], cell[1]);
+
+      if (neighbors === 2 || neighbors === 3) {
+        survivors.push(cell);
+      }
+    }
+
+    Grid.apply(grid, survivors);
+    return grid;
+  };
+
+  Grid.prototype.neighborsTo = function(x,y) {
+    var neighbors = [];
+    var dx, dy, sameCell;
+
+    for (var i = 0; i < this.liveCells.length; i++) {
+      dx = Math.abs(x-this.liveCells[i][0]);
+      dy = Math.abs(y-this.liveCells[i][1]);
+      sameCell = (dx + dy === 0);
+
+      if (dx <= 1 && dy <= 1 && !sameCell) {
+        neighbors.push(this.liveCells[i]);
+      }
+    }
+    return neighbors.length;
+  };
+
+  module.exports.Grid = Grid;
+}());
--- katas/GameOfLife/test/GameOfLife.test.js
+++ katas/GameOfLife/test/GameOfLife.test.js
@@ -7,51 +7,57 @@
   var Grid = require('./../Grid').Grid;
 
   describe('Grid', function() {
-    var grid;
-
-    beforeEach(function() {
-      grid = new Grid();
-    });
-
     it('should be defined', function() {
       Grid.should.not.be.undefined;
     });
 
     it('should be empty if created empty', function() {
-      grid.liveCells.should.be.empty;
+      new Grid().liveCells.should.be.empty;
     });
 
     it('should have the cells created with it', function() {
-      grid = new Grid([1,1], [2,2]);
-      grid.liveCells.should.eql([[1,1], [2,2]]);
+      new Grid([1,1], [2,2]).liveCells.should.eql([[1,1], [2,2]]);
     });
 
-    it('should spawn an empty board if empty', function() {
-      grid.spawn().liveCells.should.be.empty;
+    describe('spawn', function() {
+      var nextCycleCells = function() {
+        var grid = new Grid();
+        Grid.apply(grid, arguments);
+        return grid.spawn().liveCells;
+      };
+
+      it('should spawn an empty board if empty', function() {
+        nextCycleCells().should.be.empty;
+      });
+
+      it('should kill a lonely cell', function() {
+        nextCycleCells([1,1]).should.be.empty;
+      });
+
+      // @TODO: Replace eql with include (for arrays)!
+      it('should keep a cell with two neighbors', function() {
+        nextCycleCells([1,1], [2,0], [2,2]).should.eql([[1,1]]);
+      });
+
+      it('should keep a cell with three neighbors', function() {
+        nextCycleCells([1,1], [0,0], [2,0], [2,2]).should.eql([[1,1]]);
+      });
+
+      // @TODO:
+      it('should revive a cell with three neighbors', function() {
+        // nextCycleCells([0,0], [2,0], [2,2]).should.eql([[1,1]]);
+      });
     });
 
-    it('should kill a lonely cell', function() {
-      grid.spawn().liveCells.should.be.empty;
-    });
-
-    it('should keep a cell with two neighbors'
-    // @Pending: Need a way to count neighbors
-    /*
-    , function() {
-      var grid = new Grid([1,1], [2,0], [2,2]);
-      grid.spawn().liveCells.should.eql([1,1]);
-    }
-    */
-    );
+    // @TODO: Create a method that returns if the cell is alive or not
 
     describe('counting neighbors', function() {
       it('should find zero neighbors to a lonely cell', function() {
-        grid = new Grid([1,1]);
-        grid.neighborsTo(1,1).should.equal(0);
+        new Grid([1,1]).neighborsTo(1,1).should.equal(0);
       });
 
       it('should find a neighbor to a single cell', function() {
-        grid = new Grid([1,1]);
+        var grid = new Grid([1,1]);
         grid.neighborsTo(0,0).should.equal(1);
         grid.neighborsTo(0,1).should.equal(1);
         grid.neighborsTo(0,2).should.equal(1);
@@ -61,8 +67,37 @@
         grid.neighborsTo(2,1).should.equal(1);
         grid.neighborsTo(2,2).should.equal(1);
       });
+
+      it('should handle cells with two neighbors', function() {
+        var grid = new Grid([0,0], [2,2]);
+        grid.neighborsTo(1,1).should.equal(2);
+      });
+    });
+
+    describe('grid dimensions', function() {
+      var assertDimensions = function() {
+        var grid = new Grid();
+        Grid.apply(grid, arguments);
+
+        return function(dimensions) {
+          grid.width.should.equal(dimensions[0]);
+          grid.height.should.equal(dimensions[1]);
+        }
+      };
+
+      it('should have width:0 and height:0 for an empty grid', function() {
+        assertDimensions()([0,0]);
+      });
+
+      it('should have width:1 and height:1 for a single cell', function() {
+        assertDimensions([0,0])([1,1]);
+      });
+
+      it('should calculate the proper width and height for multiple cells grid', function() {
+        assertDimensions([0,0],[2,3])([3,4]);
+        assertDimensions([-1000,-1000],[1000,1000])([2001,2001]);
+        assertDimensions([0,0],[2,3],[-13,102])([16,103]);
+      });
     });
   });
-
-// 17:00
 })();
