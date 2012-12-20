aa85de648b03a97608bb88ecfbbd9c27d727ae8f
vincentsimard <vincent@vincentsimard.com>
2012-12-19 19:07:04 -0500

    Refactoring tests!

--- katas/tennis/test/tennis.test.js
+++ katas/tennis/test/tennis.test.js
@@ -12,71 +12,60 @@
     });
 
     describe('players score', function() {
+      var game;
+
+      beforeEach(function(done){
+        game = new Tennis();
+        done();
+      });
+
+      var assertScore = function(player1Score, player2Score) {
+        game.player1Score().should.equal(player1Score);
+        game.player2Score().should.equal(player2Score);
+      };
+
+      var simulateExchanges = function() {
+        var exchanges = Array.prototype.slice.call(arguments, 0);
+        var winner;
+
+        for (var i=0; i < exchanges.length; i++) {
+          winner = exchanges[i];
+          game['player'+winner+'WinsExchange']();
+        }
+      };
+
       it('should be zero at the start of the game', function() {
-        var game = new Tennis();
-        game.player1Score().should.equal(0);
-        game.player2Score().should.equal(0);
+        assertScore(0,0);
       });
 
       it('should be 15 when a player scores once', function() {
-        var game = new Tennis();
-        game.player2WinsExchange();
-        game.player2Score().should.equal(15);
+        simulateExchanges(2);
+        assertScore(0,15);
       });
 
       it('should be 15 when both players score once', function() {
-        var game = new Tennis();
-        game
-          .player1WinsExchange()
-          .player2WinsExchange();
-        game.player1Score().should.equal(15);
-        game.player1Score().should.equal(15);
+        simulateExchanges(1,2);
+        assertScore(15,15);
       });
 
       it('should be 30 when a player scores twice', function() {
-        var game = new Tennis(0, 1);
-        game
-          .player1WinsExchange()
-          .player1WinsExchange();
-        game.player1Score().should.equal(30);
+        simulateExchanges(1,1);
+        assertScore(30,0);
       });
 
       it('should be 40 when a player scores three times', function() {
-        var game = new Tennis();
-        game
-          .player1WinsExchange()
-          .player1WinsExchange()
-          .player1WinsExchange();
-        game.player1Score().should.equal(40);
+        simulateExchanges(1,1,1);
+        assertScore(40,0);
       });
+
       it('should be 40:40 when both players scores three times', function(){
-        var game = new Tennis();
-        game
-          .player1WinsExchange()
-          .player1WinsExchange()
-          .player1WinsExchange();
-        game.player1Score().should.equal(40);
-        game
-          .player2WinsExchange()
-          .player2WinsExchange()
-          .player2WinsExchange();
-        game.player2Score().should.equal(40);
+        simulateExchanges(1,1,1,2,2,2);
+        assertScore(40,40);
       });
 
       it('when the score is 40:40 and one of the players scores he gets the advantage', function(){
-        var game = new Tennis();
-        game
-          .player1WinsExchange()
-          .player1WinsExchange()
-          .player1WinsExchange()
-        
-          .player2WinsExchange()
-          .player2WinsExchange()
-          .player2WinsExchange()
-
-          .player1WinsExchange();
-
-        game.player1Score().should.equal('adv');
+        simulateExchanges(1,1,1,2,2,2,1);
+        assertScore('adv', 40);
       });
     });
   });
