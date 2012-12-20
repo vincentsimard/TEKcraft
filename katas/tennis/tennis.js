da507095bddbde2b78432ded3c65be2750e7fc94
vincentsimard <vincent@vincentsimard.com>
2012-12-19 19:37:42 -0500

    Added function to check if its the last point of the game (without deuce/adv) and changed the verbiage of some tests

--- katas/tennis/tennis.js
+++ katas/tennis/tennis.js
@@ -10,12 +10,15 @@
   };
 
   Tennis.prototype.addPointToPlayer = function(player) {
-    var index;
+    var index = GAME_PTS.indexOf(this.score[player]);
 
-    if (this.isDeuce()) {
-      this.score[player] = 'adv';
+    if (this.isLastPoint(this.score[player])) {
+      if (this.isDeuce()) {
+        this.score[player] = 'adv';
+      } else {
+        // @TODO: playerWinsGame();
+      }
     } else {
-      index = GAME_PTS.indexOf(this.score[player]);
       this.score[player] = GAME_PTS[index + 1];
     }
 
@@ -28,9 +31,16 @@
   Tennis.prototype.player1Score = function() { return this.score[PLAYER_1]; };
   Tennis.prototype.player2Score = function() { return this.score[PLAYER_2]; };
 
+  Tennis.prototype.isLastPoint = function(score) {
+    var lastPoint = GAME_PTS[GAME_PTS.length - 1];
+    return score === lastPoint;
+  };
+
   Tennis.prototype.isDeuce = function() {
-    var lastGamePoint = GAME_PTS[GAME_PTS.length - 1];
-    return this.player1Score() === lastGamePoint && this.player2Score() === lastGamePoint;
+    return (
+      this.isLastPoint(this.player1Score()) &&
+      this.isLastPoint(this.player2Score())
+    );
   };
 
   module.exports.Tennis = Tennis;
--- katas/tennis/test/tennis.test.js
+++ katas/tennis/test/tennis.test.js
@@ -11,17 +11,19 @@
       Tennis.should.not.be.undefined;
     });
 
+    // @TODO: Add the concept of game/set/match to distinguish different scores
+
     describe('players score', function() {
       var game;
 
-      beforeEach(function(done){
+      beforeEach(function(done) {
         game = new Tennis();
         done();
       });
 
-      var assertScore = function(player1Score, player2Score) {
-        game.player1Score().should.equal(player1Score);
-        game.player2Score().should.equal(player2Score);
+      var assertScore = function(score1, score2) {
+        game.player1Score().should.equal(score1);
+        game.player2Score().should.equal(score2);
       };
 
       var simulateExchanges = function() {
@@ -38,35 +40,44 @@
         assertScore(0,0);
       });
 
-      it('should be 15 when a player scores once', function() {
+      it('should be 15 when a player wins an exchange', function() {
         simulateExchanges(2);
         assertScore(0,15);
       });
 
-      it('should be 15 when both players score once', function() {
-        simulateExchanges(1,2);
-        assertScore(15,15);
-      });
-
-      it('should be 30 when a player scores twice', function() {
+      it('should be 30 when a player wins two exchanges', function() {
         simulateExchanges(1,1);
         assertScore(30,0);
       });
 
-      it('should be 40 when a player scores three times', function() {
+      it('should be 40 when a player wins three exchanges', function() {
         simulateExchanges(1,1,1);
         assertScore(40,0);
       });
 
-      it('should be 40:40 when both players scores three times', function(){
+      it('should be 15:15 when both players win one exchange', function() {
+        simulateExchanges(1,2);
+        assertScore(15,15);
+      });
+
+      it('should be 40:40 when both players win three exchanges', function(){
         simulateExchanges(1,1,1,2,2,2);
         assertScore(40,40);
       });
 
-      it('when the score is 40:40 and one of the players scores he gets the advantage', function(){
+      it('should be adv:40 when the score is 40:40 and a wins an exchange', function() {
         simulateExchanges(1,1,1,2,2,2,1);
         assertScore('adv', 40);
       });
+
+      it('should go back to 40:40 when a player having the advantage loses an exchange'
+      /*
+      @PENDING: Handle losing advantage
+      , function() {
+        simulateExchanges(1,1,1,2,2,2,1,2);
+        assertScore(40,40);
+      }*/
+      );
     });
   });
 })();
