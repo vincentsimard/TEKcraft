4e61c588b77fdb7ea1a274fb20cd187e4c1167b7
Vincent Simard <vincent@vincentsimard.com>
2012-12-22 17:56:47 -0500

    Trivial update

--- katas/tennis/test/tennis.test.js
+++ katas/tennis/test/tennis.test.js
@@ -21,6 +21,19 @@
       game.player2Score().should.equal(score2);
     });
 
+    utils.addMethod(chai.Assertion.prototype, 'deuce', function () {
+      var game = utils.flag(this, 'object');
+
+      game.should.score(40,40);
+    });
+
+    utils.addMethod(chai.Assertion.prototype, 'over', function () {
+      var game = utils.flag(this, 'object');
+      var truthy = !utils.flag(this, 'negate');
+
+      game.isOver().should.be[truthy];
+    });
+
     utils.addMethod(chai.Assertion.prototype, 'gamePoint', function () {
       var game = utils.flag(this, 'object');
       var truthy = !utils.flag(this, 'negate');
@@ -61,8 +74,28 @@
     };
 
     var simulateDeuce = function() {
-      simulateExchanges(PLAYER_1,PLAYER_1,PLAYER_1,PLAYER_2,PLAYER_2,PLAYER_2);
+      game = new Game();
+      simulateExchanges(
+        PLAYER_1,
+        PLAYER_1,
+        PLAYER_1,
+        PLAYER_2,
+        PLAYER_2,
+        PLAYER_2
+      );
     };
+
+    var simulateSweepFor = function(player) {
+      game = new Game();
+      simulateExchanges(
+        player,
+        player,
+        player,
+        player
+      );
+
+      game.should.be.over();
+    }
 
 
 
@@ -73,124 +106,122 @@
 
 
 
-    describe('game point', function() {
-      it('should not be game point if a player wins at less than 3 exchanges', function() {
-        simulateExchanges(PLAYER_1);
-        game.should.not.be.gamePoint();
-
-        simulateExchanges(PLAYER_1);
-        game.should.not.be.gamePoint();
+    describe('points', function() {
+      // Normal scoring
+      it('should be zero at the start of the game', function() {
+        game.should.score(0,0);
       });
 
-      it('should be game point if a player wins at least 3 exchanges and the other player has won at least 1 less exchanges', function() {
+      it('should be 15 when a player wins one exchange', function() {
+        simulateExchanges(PLAYER_2);
+        game.should.score(0,15);
+
+        simulateExchanges(PLAYER_1);
+        game.should.score(15,15);
+      });
+
+      it('should be 30 when a player wins two exchanges', function() {
+        simulateExchanges(PLAYER_1,PLAYER_1);
+        game.should.score(30,0);
+
+        simulateExchanges(PLAYER_2,PLAYER_2);
+        game.should.score(30,30);
+      });
+
+      it('should be 40 when a player wins three exchanges', function() {
         simulateExchanges(PLAYER_2,PLAYER_2,PLAYER_2);
-        game.should.be.gamePoint();
-        game.should.be.gamePointFor(PLAYER_2);
+        game.should.score(0,40);
+
+        simulateExchanges(PLAYER_1,PLAYER_1,PLAYER_1);
+        game.should.score(40,40);
       });
 
-      it('should not be game point if both players have won 3 exchanges', function() {
+      // Deuce
+      it('should be deuce when both players win three exchanges', function() {
         simulateDeuce();
-        game.should.not.be.gamePoint();
+        game.should.be.deuce();
       });
 
-      it('should be game point if a player wins an exchange when it is deuce', function() {
+      it('should be deuce when a player having the advantage loses an exchange', function() {
+        simulateDeuce();
+        simulateExchanges(PLAYER_1,PLAYER_2);
+        game.should.be.deuce();
+      });
+
+      it('should be deuce after a ridiculously long game where both players win exchanges back and forth', function() {
+        for (var i=0; i < 20; i++) {
+          simulateExchanges(PLAYER_1,PLAYER_2);
+        }
+        game.should.be.deuce();
+      });
+
+      // Advantages
+      it('should be advantage for a player who wins an exchange when it is deuce', function() {
         simulateDeuce();
         simulateExchanges(PLAYER_1);
-        game.should.be.gamePointFor(PLAYER_1);
+        game.should.score('adv',40);
+      });
+      
+      // Wins
+      it('should show a player has won if he wins 4 straight exchanges', function() {
+        simulateSweepFor(PLAYER_1);
+        game.should.score('win',0);
+
+        simulateSweepFor(PLAYER_2);
+        game.should.score(0,'win');
+      });
+
+      it('should show a player has won if he wins an exchange when he has the advantage', function() {
+        simulateDeuce();
+        simulateExchanges(PLAYER_2,PLAYER_2);
+        game.should.score(40,'win');
       });
     });
 
-    describe('scoring', function() {
-      describe('(normal conditions)', function() {
-        it('should be zero at the start of the game', function() {
-          game.should.score(0,0);
-        });
+    // Game point
+    it('should not be game point if a player wins less than 3 exchanges', function() {
+      simulateExchanges(PLAYER_1);
+      game.should.not.be.gamePoint();
 
-        it('should be 15 when a player wins an exchange', function() {
-          simulateExchanges(PLAYER_2);
-          game.should.score(0,15);
-        });
+      simulateExchanges(PLAYER_1);
+      game.should.not.be.gamePoint();
+    });
 
-        it('should be 30 when a player wins two exchanges', function() {
-          simulateExchanges(PLAYER_1,PLAYER_1);
-          game.should.score(30,0);
-        });
+    it('should be game point if a player wins at least 3 exchanges', function() {
+      simulateExchanges(PLAYER_2,PLAYER_2,PLAYER_2);
+      game.should.be.gamePoint();
+      game.should.be.gamePointFor(PLAYER_2);
+    });
 
-        it('should be 40 when a player wins three exchanges', function() {
-          simulateExchanges(PLAYER_1,PLAYER_1,PLAYER_1);
-          game.should.score(40,0);
-        });
+    it('should not be game point if both players have won 3 exchanges (deuce)', function() {
+      simulateDeuce();
+      game.should.not.be.gamePoint();
+    });
 
-        it('should be 15:15 when both players win one exchange', function() {
-          simulateExchanges(PLAYER_1,PLAYER_2);
-          game.should.score(15,15);
-        });
+    it('should be game point after a player wins an exchange when it is deuce', function() {
+      simulateDeuce();
+      simulateExchanges(PLAYER_1);
+      game.should.be.gamePointFor(PLAYER_1);
+    });
 
-        it('should be 40:40 when both players win three exchanges', function(){
-          simulateDeuce();
-          game.should.score(40,40);
-        });
+    it('should throw an error if players try to play when the game is over', function() {
+      simulateSweepFor(PLAYER_1);
+
+      var oneMoreExchange = (function() {
+        simulateExchanges(PLAYER_1);
       });
 
-      describe('(advantages)', function() {
-        it('should be adv:40 when the score is 40:40 and a wins an exchange', function() {
-          simulateDeuce();
-          simulateExchanges(PLAYER_1);
-          game.should.score('adv', 40);
-        });
+      oneMoreExchange.should.throw(Error); // game player 1, game is over
 
-        it('should go back to 40:40 when a player having the advantage loses an exchange', function() {
-          simulateDeuce();
-          simulateExchanges(PLAYER_1,PLAYER_2);
-          game.should.score(40,40);
-        });
+      simulateDeuce();
+      oneMoreExchange.should.not.throw(Error); // deuce
 
-        it('should go back to 40:40 after a ridiculously long game where both players win exchanges back and forth', function() {
-          simulateDeuce();
-          for (var i=0; i < 20; i++) {
-            simulateExchanges(PLAYER_1,PLAYER_2);
-          }
-          game.should.score(40,40);
-        });
-      });
-      
-      describe('(wins)', function() {
-        it('should declare a player has won if he wins 4 straight exchanges', function() {
-          simulateExchanges(PLAYER_1,PLAYER_1,PLAYER_1,PLAYER_1);
-          game.should.score('win',0);
-
-          game = new Game();
-          simulateExchanges(PLAYER_2,PLAYER_2,PLAYER_2,PLAYER_2);
-          game.should.score(0,'win');
-        });
-
-        it('should declare a player has won if he wins an exchange when he has the advantage', function() {
-          simulateDeuce();
-          simulateExchanges(PLAYER_2,PLAYER_2);
-          game.should.score(40,'win');
-        });
-
-        it('should throw an error if players try to play when the game has already been won', function() {
-          simulateExchanges(PLAYER_1,PLAYER_1,PLAYER_1,PLAYER_1);
-
-          var oneMoreExchange = (function() {
-            simulateExchanges(PLAYER_1);
-          });
-
-          oneMoreExchange.should.throw(Error); // game player 1, game is over
-
-          game = new Game();
-          simulateDeuce();
-          oneMoreExchange.should.not.throw(Error); // deuce
-
-          simulateExchanges(PLAYER_2,PLAYER_1,PLAYER_2,PLAYER_1,PLAYER_1);
-          oneMoreExchange.should.throw(Error); // game player 1, game is over
-        });
-      });
+      simulateExchanges(PLAYER_2,PLAYER_1,PLAYER_1);
+      oneMoreExchange.should.throw(Error); // game player 1, game is over
     });
 
     describe('score()', function() {
-      it('should translate 0, 15, 30, 40', function() {
+      it('should translate points (0, 15, 30, 40)', function() {
         simulateExchanges(PLAYER_1);
         game.score().should.equal('fifteen, love');
 
@@ -211,7 +242,7 @@
         game.score().should.equal('thirty all');
       });
 
-      it('should translate deuce', function() {
+      it('should translate "40 all" to "deuce"', function() {
         simulateDeuce();
         game.score().should.equal('deuce');
       });
@@ -226,11 +257,10 @@
       });
 
       it('should use "game #{PLAYER}" for won game', function() {
-        simulateExchanges(PLAYER_1,PLAYER_1,PLAYER_1,PLAYER_1);
+        simulateSweepFor(PLAYER_1);
         game.score().should.equal('game Player 1');
 
-        game = new Game();
-        simulateExchanges(PLAYER_2,PLAYER_2,PLAYER_2,PLAYER_2);
+        simulateSweepFor(PLAYER_2);
         game.score().should.equal('game Player 2');
       });
     });
